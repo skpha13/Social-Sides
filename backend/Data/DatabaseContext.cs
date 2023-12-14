c8f239dd9588c2992c8aba58989a630c2a03afeb
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-12-14 14:38:27 +0200

    feat: connected database

---
+++ backend/Data/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace backend.Data
+{
+	public class DatabaseContext : DbContext
+	{
+		public DbSet<Class> Class { get; set; }
+
+		public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
