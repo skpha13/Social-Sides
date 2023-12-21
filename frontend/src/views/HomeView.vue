504d1f77765f7d6d91a93f8c9bec9100fe0bcf8f
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2023-12-21 20:09:23 +0200

    feat: done theme color switch

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,6 +4,6 @@
 
 <template>
   <div>
-    <h1 class="text-2xl">Home</h1>
+    <h1 class="text-2xl text-text">Home</h1>
   </div>
 </template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // TODO: define colors
+        background: {
+          default: "var(--color-background)",
+          soft: "var(--color-background-soft)",
+          mute: "var(--color-background-mute)"
+        },
+        border: {
+          default: "var(--color-border)",
+          hover: "var(--color-border-hover)"
+        },
+        heading: "var(--color-heading)",
+        text: "var(--color-text)",
+      }
+    },
+  },
+  variants: {
+    extend: {
+      // backgroundColor: ['dark'], // Add dark mode variant for background color
+      // borderColor: ['dark'], // Add dark mode variant for border color
+      // textColor: ['dark'], // Add dark mode variant for text color
+      // Add more variants as needed
+    },
   },
   plugins: [],
 }
