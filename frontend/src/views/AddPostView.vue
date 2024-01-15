2f0c65f9705de607abf9976ce3927215d46f52cd
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2024-01-15 12:53:51 +0200

    feat: formit with text and category

--- frontend/src/views/AddPostView.vue
+++ frontend/src/views/AddPostView.vue
@@ -1,8 +1,62 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { useToast } from 'vue-toastification'
+import { ref } from 'vue'
+import { Category, type ICategory } from '@/models/Category'
+
+const toast = useToast();
+
+interface IOptions {
+  label: string,
+  value: string
+}
+
+// ======== CATEGORY FETCHING ========
+const categories = ref<ICategory[]>([]);
+const options = ref<IOptions[]>([]);
+const areCategoriesLoaded = ref(false);
+
+const categoryWorker = new Category();
+const fetchCategories = async () => {
+  categories.value = await categoryWorker.all();
+  options.value = categories.value.map((iterator) => {
+    return {
+      label: iterator.title,
+      value: iterator.id
+    }
+  });
+  areCategoriesLoaded.value = true;
+}
+
+fetchCategories();
+// ====================================
+
+const addingPostHandler = async () => {
+
+}
 
 </script>
 
 <template>
   <PageTitle title="Add Post" />
+  <FormKit v-if="areCategoriesLoaded" type="form" @submit="addingPostHandler" submit-label="Add Post">
+    <FormKit
+      type="textarea"
+      label="Text"
+      placeholder="Say something"
+      name="text"
+      validation="required"
+      validation-visibility="live"
+    />
+
+    <FormKit
+      type="select"
+      label="Category"
+      placeholder="Select a category"
+      name="categoryId"
+      :options="options"
+      validation="required"
+      validation-visibilty="live"
+    />
+  </FormKit>
 </template>
