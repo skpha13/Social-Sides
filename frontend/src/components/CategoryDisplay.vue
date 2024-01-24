e229582a539883d11bf2336437bfd9dee7221fe2
Adrian Mincu <61752094+skpha13@users.noreply.github.com>
2024-01-24 17:34:59 +0200

    feat: implemented post filtering by category

--- frontend/src/components/CategoryDisplay.vue
+++ frontend/src/components/CategoryDisplay.vue
@@ -4,10 +4,15 @@ const props = defineProps<{
   title: string
   color: string
 }>();
+
+const emit = defineEmits<{
+  (e: 'filter-by-category', id:string): void
+}>()
 </script>
 
 <template>
   <button
+    @click="emit('filter-by-category', props.id)"
     class="text-sm text-center
             border rounded-xl py-0.5 px-4 mx-4 min-w-[80px]
             hover:backdrop-brightness-200
